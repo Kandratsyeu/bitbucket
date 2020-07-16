fba031b97274ca9dc74388e7980ed08fcb236da6
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2020-07-16 13:22:54 +0300

    session cookie database check

--- autorization.php
+++ autorization.php
@@ -1,30 +1,31 @@
 <?php 
 session_start();
 $customer = json_decode($_POST["params"], true);
-$userList = simplexml_load_file('db.xml');
 if ($customer) {
 	if (file_exists('db.xml')) {
-    foreach ($userList->user as $user) {
-		if ((string)$user->login == $customer['login']) {
-			if ((string)$user->password == md5($customer['pass'])) {
-				echo (string)$user->name;
-				$_SESSION['user_name'] = (string)$user->name;
-				$_SESSION['user_login'] = (string)$user->login;
-				$_SESSION['users_db'] = 'db.xml'; 
-				exit();
-			} else {
-				echo 0;
-				exit();
+		$userList = simplexml_load_file('db.xml');
+	    foreach ($userList->user as $user) {
+			if ((string)$user->login == $customer['login']) {
+				if ((string)$user->password == md5($customer['pass'])) {
+					echo (string)$user->name;
+					$user->addChild('cookie', $_COOKIE['PHPSESSID']);
+					$_SESSION['user_name'] = (string)$user->name;
+					$_SESSION['user_login'] = (string)$user->login;
+					$_SESSION['user_cookie'] = (string)$user->cookie;
+					exit();
+				} else {
+					echo 0;
+					exit();
+				}
 			}
 		}
-	}
-	echo 0;
+		echo 0;
 	} else {
 	    echo 'Заполните форму';
 	}
 }
 
-if (isset($_SESSION['user_login'])) {
+if (isset($_SESSION['user_login']) && $_COOKIE['PHPSESSID']==$_SESSION['user_cookie']) {
 	echo $_SESSION['user_name'];
 } else {
 	echo 0;
